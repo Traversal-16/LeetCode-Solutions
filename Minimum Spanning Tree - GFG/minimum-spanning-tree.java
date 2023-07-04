3a6e65796acc3dfe14c1839a9e044c9e6445d422
Shivanshu Katiyar <137396324+Traversal-16@users.noreply.github.com>
2023-07-04 06:06:34 +0530

    Added solution - LeetHub

---
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main{
+	static BufferedReader br;
+	static PrintWriter ot;
+    public static void main(String args[]) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-- > 0){
+			String s[] = br.readLine().trim().split(" ");
+			int V = Integer.parseInt(s[0]);
+			int E = Integer.parseInt(s[1]);
+			int edges[][] = new int[E][3];
+			for(int i = 0; i < E; i++){
+				s = br.readLine().trim().split(" ");
+				edges[i][0] = Integer.parseInt(s[0]);
+				edges[i][1] = Integer.parseInt(s[1]);
+				edges[i][2] = Integer.parseInt(s[2]);
+			}
+			ot.println(new Solution().spanningTree(V, E, edges));
+		}
+		ot.close();
+	}
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Pair
+{
+    int distance;
+    int node;
+    Pair (int a,int b)
+    {
+        distance=a;
+        node=b;
+    }
+}
+class Solution{
+	static int spanningTree(int V, int E, int edges[][])
+	{
+	    // Code Here. 
+	    PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->(a.distance-b.distance));
+	    boolean vis[]=new boolean [V];
+	    ArrayList<ArrayList<Pair>> adj=new ArrayList <>();
+	    for (int i=0;i<V;i++)
+	    {
+	        adj.add (new ArrayList <>());
+	    }
+	    for (int i=0;i<E;i++)
+	    {
+	        adj.get (edges[i][0]).add (new Pair (edges[i][2],edges[i][1]));
+	        adj.get (edges[i][1]).add (new Pair (edges[i][2],edges[i][0]));
+	    }
+	    int sum=0;
+	    pq.offer (new Pair (0,0));
+	    while (!pq.isEmpty ())
+	    {
+	        Pair p=pq.poll();
+	        if (vis[p.node]==true) continue;
+	        
+	        sum+=p.distance;
+	        vis[p.node]=true;
+	        
+	        int dis=p.distance;
+	        int node=p.node;
+	        for (Pair t:adj.get(node))
+	        {
+	            if (vis[t.node]==false)
+	            {
+	                pq.offer (new Pair(t.distance,t.node));
+	            }
+	        }
+	    }
+	    return sum;
+	}
+}
